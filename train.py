1ddb29a75a1ed357f6be02d063c03692202eb38d
Maulik bhardwaj <sharmamaulik816@gmail.com>
2026-05-05 17:44:46 +0530

    train.py

--- train.py
+++ train.py
@@ -1,18 +1,17 @@
 from ultralytics import YOLO
 
-# Pretrained model load karo (Transfer Learning base)
+
 model = YOLO("yolov8n.pt")
 
-# Fine-tuning on custom obstacle detection dataset
 results = model.train(
     data=r"C:\Users\mauli\Downloads\Obstacle detection.v1i.yolov8\data.yaml",
     epochs=30,
     imgsz=640,
-    freeze=10,        # Early 10 layers freeze = proper Transfer Learning
+    freeze=10,       
     batch=16,
     name="blind_assist_finetuned",
     patience=5,
-    device=0          # GPU use karega agar available ho, warna CPU
+    device=0          #
 )
 
 print("\n" + "="*50)
